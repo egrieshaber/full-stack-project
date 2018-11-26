c27c027b30488f4675c878c2a33bce2963f8fae1
egrieshaber <eric.j.grieshaber>
2018-11-26 15:32:11 -0500

    11/26/18 03:32pm
    updated curl scripts to heroku address

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "http://localhost:4741/sign-in" \
+curl "https://metalhedz.herokuapp.com/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
